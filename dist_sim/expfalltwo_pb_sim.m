d2565b42fa7dbdb770db9f454db48f59f755f9d3
Jeff Gelles <gelles@brandeis.edu>
2015-12-17 12:59:30 -0500

    Added simulations of 2exp distributions +/- photobleaching

---
+++ dist_sim/expfalltwo_pb_sim.m
@@ -0,0 +1,29 @@
+function dwellts=expfalltwo_pb_sim(ap, tau1, tau2, pb_tau, n)
+% Generates dwell times drawn from a biexponential pdf with exponential
+% photobleaching
+%% 
+% Copyright 2015 Jeff Gelles, Brandeis University 
+% This is licensed software; see notice at end of file. 
+%%
+% Parameters:
+%  ap      amplitude fraction
+%  tau1    time constants
+%  tau2 
+%  pb_tau  photobleaching time constant
+%  n       number of dwells to generate
+%%
+rawdwellts=expfalltwo_sim(ap, tau1, tau2, n)
+pbts = random('exp',pb_tau,[n, 1])
+dwellts=min(rawdwellts,pbts)
+end
+%% notice
+% This is free software: you can redistribute it and/or modify it under the
+% terms of the GNU General Public License as published by the Free Software
+% Foundation, either version 3 of the License, or (at your option) any later
+% version.
+
+% This software is distributed in the hope that it will be useful, but WITHOUT ANY
+% WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+% A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+% You should have received a copy of the GNU General Public License
+% along with this software. If not, see <http://www.gnu.org/licenses/>.
---
+++ dist_sim/expfalltwo_sim.m
@@ -0,0 +1,31 @@
+function dwellts=expfalltwo_sim(ap, tau1, tau2, n)
+% Generates dwell times drawn from a biexponential pdf 
+%% 
+% Copyright 2015 Jeff Gelles, Brandeis University 
+% This is licensed software; see notice at end of file. 
+%%
+% Parameters:
+%  ap      amplitude fraction
+%  tau1    time constants
+%  tau2 
+%  n       number of dwells to generate
+%%
+mus(1) = tau1;
+mus(2) = tau2;
+component=(rand(n,1) > ap) + 1;
+dwellts=zeros(n,1);
+for i = 1:n
+    dwellts(i) = random('exp',mus(component(i)));
+end
+
+%% notice
+% This is free software: you can redistribute it and/or modify it under the
+% terms of the GNU General Public License as published by the Free Software
+% Foundation, either version 3 of the License, or (at your option) any later
+% version.
+
+% This software is distributed in the hope that it will be useful, but WITHOUT ANY
+% WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+% A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+% You should have received a copy of the GNU General Public License
+% along with this software. If not, see <http://www.gnu.org/licenses/>.
