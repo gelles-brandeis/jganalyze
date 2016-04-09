684c2b24e8698344fb3f0f6ee6822bf820287643
Jeff Gelles <gelles@brandeis.edu>
2016-04-09 03:40:52 -0400

    Modified Kernel_pdf to optionally handle non-positive distributions

--- plot/plot_histo/kernel_pdf.m
+++ plot/plot_histo/kernel_pdf.m
@@ -1,28 +1,42 @@
-function [yi, l_ci, u_ci] = kernel_pdf(dwells, plot_x, nboot, ci)
+function [yi, l_ci, u_ci] = kernel_pdf(dwells, plot_x, nboot, ci, all_pos)
 % constructs a normal kernel pdf of vector dwells suitable for plotting
 %
 % dwells - dwell time vector
 % plot_x - values along x-axis of hist at which to compute histo
 % nboot - number of bootstrap evaluations for c.i. calc; 0 = don't calc c.i.
 % confidence interval width 0 < ci < 100
+% all_pos - if true (default) all dwells are postitive (the usual case!)
 %
 % yi - histo values
 %%
 % Copyright 2016 Jeff Gelles, Brandeis University.
 % This is licensed software; see notice at end of file. 
 %%
-[yi, ~] = ksdensity(dwells,plot_x,'support','positive');
+if nargin == 4
+    all_pos = true;
+end
+if all_pos
+    support = 'positive';
+else
+    support = 'unbounded';
+end
+
+[yi, ~] = ksdensity(dwells, plot_x, 'support', support);
+
 if nboot > 0
     %bootstrap it
     boot_results = zeros(nboot, length(plot_x));
     boot_dwells = zeros(length(dwells),1);
     parfor n = 1:nboot
         boot_dwells = dwells(randi(length(dwells), length(dwells), 1));
-        boot_results(n,:) = ksdensity(boot_dwells,plot_x,'support','positive');
+        boot_results(n,:) = ksdensity(boot_dwells,plot_x,'support',support);
     end
     l_ci = prctile(boot_results, (100-ci)./2);
     u_ci = prctile(boot_results, 100 - ((100-ci)./2));
 end
+%% versions
+% version 2 4/9/16 JG
+%   added all_pos parameter
 %% notice
 % This is free software: you can redistribute it and/or modify it under the
 % terms of the GNU General Public License as published by the Free Software
