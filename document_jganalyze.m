40cf30aa6aba366fb857970a5270fd1a71d0676d
Jeff Gelles <gelles@brandeis.edu>
2020-04-07 17:11:27 -0400

    Update documentation

--- document_jganalyze.m
+++ document_jganalyze.m
@@ -2,7 +2,7 @@
 
 % m2html is at http://www.artefact.tk/software/matlab/m2html/
 % this script assumes m2html is installed at:
-addpath('C:\Users\gelles\Documents\MATLAB\m2html')
+addpath('C:\Users\gelles\Documents\Box Sync\JGDocs\Computation\MATLAB\m2html')
 cd ('C:\Users\gelles\Documents\GitHub')
 warning('off')
 m2html('mfiles','jganalyze', 'htmldir','jganalyze/documentation', ...
